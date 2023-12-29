532635692d079601536644ee8aef0f41d8de8180
Artyom Romanov <artem.romanov.03@bk.ru>
2023-12-29 21:14:46 +0500

    Add posts tests
    Signed-off-by: Artyom Romanov <artem.romanov.03@bk.ru>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from starlette.testclient import TestClient
 
 from src.social_network.app import app
 from src.social_network.db.session import engine, get_session
+from src.social_network.schemas.auth import CreateUserAuthSchema
 
 
 @pytest.fixture()
@@ -25,3 +26,17 @@ def test_client(db_session):
     app.dependency_overrides[get_session] = lambda: db_session
     test_client = TestClient(app)
     return test_client
+
+
+@pytest.fixture()
+def auth_headers(test_client):
+    auth_schema = CreateUserAuthSchema(
+        email='test_email@mail.com',
+        username='test_username',
+        name='test_name',
+        surname='test_surname',
+        password='test_password',
+    )
+    response = test_client.post('/auth/sign-up', json=auth_schema.model_dump())
+    auth_token = response.headers.get('set-cookie')
+    return {'Cookie': auth_token}
